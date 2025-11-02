b280784d35953d1958dadabd175894c71f0efe73
Tyvian10 <binanaapuassa@outlook.com>
2025-11-03 00:24:04 +0100

    Verbeterde versie Investment, en sparen , ProfilePage,

--- BankApp/BankApp_WPF/Investment.xaml.cs
+++ BankApp/BankApp_WPF/Investment.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
 
 namespace BankApp_WPF
 {
-    internal class Investment
+    public partial class Investment : Window
     {
+        public Investment()
+        {
+            InitializeComponent();
+        }
+
+        // Terug knop bovenaan
+        private void BackButtonTop_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Terug knop onderaan
+        private void BackButtonMain_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- BankApp/BankApp_WPF/ProfilePage.xaml.cs
+++ BankApp/BankApp_WPF/ProfilePage.xaml.cs
@@ -1,12 +1,66 @@
 ﻿using System.Windows;
 
-namespace BankApp_WPF  // Moet BankApp_WPF zijn!
+namespace BankApp_WPF
 {
     public partial class ProfilePage : Window
     {
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        // Terug knop - sluit het venster
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Voice toggle knop
+        private void VoiceToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Toggle voice functionaliteit
+            if (VoiceToggleButton.Content.ToString() == "🔊")
+            {
+                VoiceToggleButton.Content = "🔇";
+                MessageBox.Show("Spraak feedback uitgeschakeld", "Voice", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                VoiceToggleButton.Content = "🔊";
+                MessageBox.Show("Spraak feedback ingeschakeld", "Voice", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        // Opslaan knop
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Validatie: Check of wachtwoorden overeenkomen
+            if (!string.IsNullOrEmpty(PasswordBox.Password) &&
+                PasswordBox.Password != ConfirmPasswordBox.Password)
+            {
+                MessageBox.Show("Wachtwoorden komen niet overeen!", "Fout", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Profiel opslaan
+            MessageBox.Show("Profiel succesvol bijgewerkt!", "Succes", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        // Verwijder account knop
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show(
+                "Weet u zeker dat u uw profiel wilt verwijderen?\n\nDeze actie kan niet ongedaan gemaakt worden!",
+                "Waarschuwing",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning
+            );
+
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Account succesvol verwijderd", "Verwijderd", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
         }
     }
 }
