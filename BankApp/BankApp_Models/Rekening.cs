a991c2587136440fb6d35ceebe1564c6d21db09c
TimMolineaux <tim.molineaux@gmail.com>
2025-11-02 19:31:12 +0100

    Views voor spaarrekeningen + database configuratie + datamodels aanmaken

---
+++ BankApp/BankApp_Models/Rekening.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankApp_Models
+{
+    public enum RekeningType
+    {
+        Zicht,
+        Spaar
+    }
+
+    public class Rekening
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(34)]
+        public string Iban { get; set; } = string.Empty;
+
+        [Required]
+        public RekeningType Type { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Saldo { get; set; }
+
+        // 🔹 Foreign key
+        public int GebruikerId { get; set; }
+        public Gebruiker Gebruiker { get; set; }
+
+        // 🔹 Relatie met Transacties
+        public ICollection<Transactie> TransactiesVan { get; set; } = new List<Transactie>();
+        public ICollection<Transactie> TransactiesNaar { get; set; } = new List<Transactie>();
+    }
+}
---
+++ BankApp/BankApp_Models/Transactie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankApp_Models
+{
+    public class Transactie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string VanIban { get; set; } = string.Empty;
+
+        [Required]
+        public string NaarIban { get; set; } = string.Empty;
+
+        public string NaamOntvanger { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Bedrag { get; set; }
+
+        public string Omschrijving { get; set; } = string.Empty;
+
+        public DateTime Datum { get; set; }
+
+        // 🔹 Optionele FK naar Gebruiker (wie voerde uit)
+        public int? GebruikerId { get; set; }
+        public Gebruiker? Gebruiker { get; set; }
+    }
+}
+
