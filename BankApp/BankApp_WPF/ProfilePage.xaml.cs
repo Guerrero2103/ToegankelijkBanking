5fee3cf6b9951e1ae40fa9c629eda27e28cc0a1c
Tyvian10 <binanaapuassa@outlook.com>
2025-11-02 18:44:06 +0100

    Profilepage, en investmentpage

---
+++ BankApp/BankApp_WPF/ProfilePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace BankApp_WPF  // Moet BankApp_WPF zijn!
+{
+    public partial class ProfilePage : Window
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
